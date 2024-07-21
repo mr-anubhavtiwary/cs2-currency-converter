6c36b8ecbf557a38e587174cdd6c1970729435df
Anubhav <mr.anubhavtiwary@gmail.com>
2024-07-21 15:34:07 +0530

    npm

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import Freecurrencyapi from '@everapi/freecurrencyapi-js';
 
 const freecurrencyapi = new Freecurrencyapi('fca_live_8STL8frUBHkmfrh6AyUwz1ET8lMIrEZGudQTLCbD');
 
-// convertCurrency("INR", "USD", 3);
-
 export async function convertCurrency(fromCurrency, toCurrency, units){
     const res = await freecurrencyapi.latest({
         base_currency: fromCurrency,
